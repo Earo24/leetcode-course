452285cb417e2e22ff3fc92ceaae39abcf3ebc4c
pengyichen <pengyichen@ysten.com>
2020-07-22 18:52:49 +0800

    update

--- 14.最长公共前缀.java
+++ 14.最长公共前缀.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * @lc app=leetcode.cn id=14 lang=java
  *
@@ -5,16 +8,54 @@
  */
 
 // @lc code=start
-class Solution1 {
-    public static void main(String[] args) {
-        String s="flower";
-        char[] ca=s.toCharArray();
-        System.out.println(ca[0]+1);
-        System.out.println(ca[1]+1);
-        System.out.println(101^101^101);
-    }
+class Solution {
     public String longestCommonPrefix(String[] strs) {
-        return "";
+        // if(strs.length==0){
+        //     return "";
+        // }
+        // List<char[]> list=new ArrayList<>();
+        // for(String str:strs){
+        //     list.add(str.toCharArray());
+        // }
+        // int i=0;
+        // out:while(true){
+        //     int res=0;
+        //     for(char[] str:list){
+        //         if(i>str.length-1){
+        //             break out;
+        //         }
+        //         res=res^str[i];
+        //         if(res!=0&&res!=str[i]){
+        //             break out;
+        //         }
+        //     }
+        //     if(strs.length%2==0){
+        //         if(res!=0){
+        //             break;
+        //         }
+        //     }else{
+        //         if(res!=list.get(0)[i]){
+        //             break;
+        //         }
+        //     }
+        //     i++;
+        // }
+        // return strs[0].substring(0,i);
+        if(strs.length==0){
+            return "";
+        }
+        String pStr=strs[0];
+        for(int i=0;i<strs.length;i++){
+            int j=0;
+            for(;j<strs[i].length()&&j<pStr.length();j++){
+                if(pStr.charAt(j)!=strs[i].charAt(j)){
+                    break;
+                }
+            }
+            pStr=pStr.substring(0,j);
+        }
+        return pStr;
     }
 }
 // @lc code=end
+
